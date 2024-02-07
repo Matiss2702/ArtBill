cf3be3fca62794354d40c5fc16bfefbd91bf00d5
janparacha <parmusta@gmail.com>
2024-02-07 18:32:53 +0100

    Invoice

--- src/DataFixtures/QuotationFixtures.php
+++ src/DataFixtures/QuotationFixtures.php
@@ -13,22 +13,22 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class QuotationFixtures extends Fixture implements DependentFixtureInterface
 
 {
+    public const QUOTATION = 'Devis';
+
     private $quotationStatus = ['created', 'sent', 'refused', 'accepted', 'paid', 'expired'];
 
     public function load(ObjectManager $manager): void
     {
-
-        for ($i = 0; $i < 10; $i++) {
-            $quotation = new Quotation();
-            $quotation->setDescription('description ' . $i);
-            $quotation->setStatus($this->quotationStatus[array_rand($this->quotationStatus)]);
-            $quotation->setDate(new \DateTime());  // Date de création
-            $quotation->setOwner($this->getReference(UserFixtures::ADMIN_USER));
-            $quotation->setCompany($this->getReference(CompanyFixtures::COMPANY_GRAPHIKART));
-
-            $manager->persist($quotation);
-        }
+        $quotation = new Quotation();
+        $quotation->setDescription('description ');
+        $quotation->setStatus($this->quotationStatus[array_rand($this->quotationStatus)]);
+        $quotation->setDate(new \DateTime());  // Date de création
+        $quotation->setOwner($this->getReference(UserFixtures::ADMIN_USER));
+        $quotation->setCompany($this->getReference(CompanyFixtures::COMPANY_GRAPHIKART));
+        $manager->persist($quotation);
         $manager->flush();
+
+        $this->addReference(self::QUOTATION, $quotation);
     }
 
     public function getDependencies()
