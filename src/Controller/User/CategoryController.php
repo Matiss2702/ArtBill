8a01dd33ac9c075ed1f180c66f5a5f07374839bd
nikolasbaele <nikolasbaele@gmail.com>
2024-03-03 22:57:21 +0100

    suppression des modifs pour role user + ajout logout

--- src/Controller/User/CategoryController.php
+++ src/Controller/User/CategoryController.php
@@ -24,31 +24,6 @@ class CategoryController extends AbstractController
             'categories' => $categoryRepository->findAll(),
         ]);
     }
-    //Ajout
-
-    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if (null === $category->getCreated()) {
-                $category->setCreated(new \DateTime());
-            }
-
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('user_category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/category/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
 
     #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Category $category): Response
@@ -56,34 +31,5 @@ class CategoryController extends AbstractController
         return $this->render('user/category/show.html.twig', [
             'category' => $category,
         ]);
-    }
-    //Modification
-    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-    //Suppression
-    #[Route('/{id}', name: 'delete', methods: ['POST'])]
-    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('index', [], Response::HTTP_SEE_OTHER);
     }
 }
