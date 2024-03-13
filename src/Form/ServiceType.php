edc906a49dd02bb57e34430c80f43a1324671548
Matiss2702 <matiss.haillouy@gmail.com>
2024-03-13 17:55:29 +0000

    add class serice input

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -17,33 +17,33 @@ class ServiceType extends AbstractType
         $builder
             ->add('label', null, [
                 'attr' => [
-                    'class' => 'w-full',
+                    'class' => 'w-full label-presta',
                 ],
                 'label' => 'Libellé',
              ])
             ->add('description', null, [
                 'attr' => [
-                    'class' => 'w-full'
+                    'class' => 'w-full description-presta'
                 ]
             ])
             ->add('price', null, [
                 'attr' => [
                     'step' => 0.01,
-                    'class' => 'w-full'
+                    'class' => 'w-full price-presta'
                 ],
                 'label' => 'Prix',
 
             ])
             ->add('quantity', null, [
                 'attr' => [
-                    'class' => 'w-full'
+                    'class' => 'w-full quantity-presta'
                 ],
                 'label' => 'Quantité',
             ])
             ->add('vatRate', ChoiceType::class, [
                 'choices' => array_combine(["0%", "10%", "20%"], Service::VAT_RATES),
                 'attr' => [
-                    'class' => 'w-full'
+                    'class' => 'w-full vat-presta'
                 ],
                 'label' => 'Taux de TVA',
 
@@ -52,7 +52,7 @@ class ServiceType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'label',
                 'attr' => [
-                    'class' => 'w-full'
+                    'class' => 'w-full category-presta'
                 ],
                 'label' => 'Catégories',
 
